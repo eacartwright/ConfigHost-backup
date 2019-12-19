8b0267fc3296e80b23ca0506dfd84a334260ce67
Evan Cartwright <eacartwright@gmail.com>
2019-12-18 21:06:07 -0500

    huh

---
+++ NewFunc.ps1
@@ -0,0 +1,27 @@
+    param (
+        $mode
+    )
+
+    $source = "[$($MyInvocation.MyCommand)]"
+    LogMessage 6 $source $mode
+
+    switch ($mode) {
+        0 {
+
+        }
+        1 {
+
+        }
+        '?' {
+            switch (().Data) {
+                1 {
+                    $script:FunctionVariable = 1
+                }
+                default {
+                    $script:FunctionVariable = 0
+                }
+            }
+            LogMessage 6 $source $FunctionVariable
+            return $FunctionVariable
+        }
+    }
