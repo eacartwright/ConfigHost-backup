95213998fe4ca5f6360f08ad114040e2a55afae6
Evan <eacartwright@gmail.com>
2019-12-23 00:24:38 -0500

    .

--- Extras/funclist.ps1
+++ Extras/funclist.ps1
@@ -1,2 +1,5 @@
 $scriptPath = 'C:\Dev\ConfigHost\ConfigHost.ps1'
-( Get-ChildItem -Path $scriptPath -Recurse | Select-String -Pattern 'function' ) | %{ $_.Line.Substring(9) } | Out-File -FilePath "$(Split-Path $scriptPath)\FuncList.txt"
+(Get-ChildItem -Path $scriptPath -Recurse |
+    Select-String -Pattern 'function') |
+    ForEach-Object { $_.Line.Substring(9) } |
+    Out-File -FilePath "$(Split-Path $scriptPath)\FuncList.txt"
